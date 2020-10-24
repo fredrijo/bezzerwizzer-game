85bd8c12994e6556d1293e3ab124bd0932efcfe4
Fredrik Jørgense <fredrik.jorgensen@kolonial.no>
2020-10-24 14:05:33 +0200

    Add Haugenstua

--- src/Game.js
+++ src/Game.js
@@ -131,6 +131,8 @@ export default class Game extends React.Component {
                     <NewRoundButton players={this.state.players} onClick={this.newRound.bind(this)} />
                     <Music text="Lag litt stemning" mp3="kvitt_eller_dobbelt" />
                     <Music text="Lag masse stemning" mp3="sandstorm" />
+                    <Music text="H4U93N57U4" mp3="Haugenstua" />
+
                 </div>
                 <div className="game">
                     <GameBoard
